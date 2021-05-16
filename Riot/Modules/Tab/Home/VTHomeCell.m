0fbe692a0d3e111ca34fb84d7c4714c4a44ee96f
Vitta <wanglikun@shinetechchina.com>
2021-05-16 12:47:15 +0800

    feat(home):add new home page UI(WIP)

--- Riot/Modules/Tab/Home/VTHomeCell.m
+++ Riot/Modules/Tab/Home/VTHomeCell.m
@@ -19,8 +19,8 @@
 
 @implementation VTHomeCell
 
-- (instancetype)initWithFrame:(CGRect)frame {
-    self = [super initWithFrame:frame];
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         [self setupUI];
     }
