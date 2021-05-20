a58bbe7878bb6d9475f1b614b8051c2690790ec2
Vitta <wlkhn123@gmail.com>
2021-05-20 14:01:42 +0800

    feat(appdelegate): adjust launch app logic

--- Riot/Modules/Application/AppDelegate.swift
+++ Riot/Modules/Application/AppDelegate.swift
@@ -14,13 +14,12 @@
 // limitations under the License.
 //
 
-import UIKit
-import PushKit
 import IQKeyboardManagerSwift
+import PushKit
+import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
     // MARK: - Properties
 
     // MARK: Private
@@ -31,9 +30,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     private var legacyAppDelegate: LegacyAppDelegate {
         return AppDelegate.theDelegate()
     }
-    
+
     // MARK: Public
-    
+
     /// Call the Riot legacy AppDelegate
     @objc class func theDelegate() -> LegacyAppDelegate {
         guard let legacyAppDelegate = LegacyAppDelegate.the() else {
@@ -41,102 +40,102 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         return legacyAppDelegate
     }
-    
+
     // UIApplicationDelegate properties
-    
+
     /// Main application window
     var window: UIWindow?
-    
+
     // MARK: - UIApplicationDelegate
-    
+
     // MARK: Life cycle
-    
+
     func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-        return self.legacyAppDelegate.application(application, willFinishLaunchingWithOptions: launchOptions)
+        return legacyAppDelegate.application(application, willFinishLaunchingWithOptions: launchOptions)
     }
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         IQKeyboardManager.shared.enable = true
-        
+
         Common.initLanguage()
         // Setup window
         let window = UIWindow(frame: UIScreen.main.bounds)
         self.window = window
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(changeLanguage), name: NSNotification.Name(rawValue: kChangeLanguageNotify), object: nil)
-        
-        // Create AppCoordinator
-        self.rootRouter = RootRouter(window: window)
-        
-        let appCoordinator = AppCoordinator(router: self.rootRouter)
-        appCoordinator.start()
-        self.legacyAppDelegate.delegate = appCoordinator
-        
-        self.appCoordinator = appCoordinator
-        
-        // Call legacy AppDelegate
-        self.legacyAppDelegate.window = window
-//        self.legacyAppDelegate.application(application, didFinishLaunchingWithOptions: launchOptions)
 
-        let loginController: VTLoginViewController = VTLoginViewController()
-        self.window?.rootViewController = loginController
-        self.window?.makeKeyAndVisible()
+//        NotificationCenter.default.addObserver(self, selector: #selector(changeLanguage), name: NSNotification.Name(rawValue: kChangeLanguageNotify), object: nil)
+
+        // Create AppCoordinator
+        rootRouter = RootRouter(window: window)
+
+        let appCoordinator = AppCoordinator(router: rootRouter)
+        appCoordinator.start()
+        legacyAppDelegate.delegate = appCoordinator
+
+        self.appCoordinator = appCoordinator
+
+        // Call legacy AppDelegate
+        legacyAppDelegate.window = window
+        legacyAppDelegate.application(application, didFinishLaunchingWithOptions: launchOptions)
+
+//        let loginController: VTLoginViewController = VTLoginViewController()
+//        self.window?.rootViewController = loginController
+//        self.window?.makeKeyAndVisible()
         return true
     }
-    
+
     @objc func changeLanguage() {
         let rootVC: UIViewController = UIApplication.shared.keyWindow?.rootViewController ?? UIViewController()
         let loginController: VTLoginViewController = VTLoginViewController()
         UIApplication.shared.delegate?.window??.rootViewController = loginController
         UIApplication.shared.delegate?.window??.makeKeyAndVisible()
     }
-    
+
     func applicationDidBecomeActive(_ application: UIApplication) {
-        self.legacyAppDelegate.applicationDidBecomeActive(application)
+        legacyAppDelegate.applicationDidBecomeActive(application)
     }
-    
-    func applicationWillResignActive(_ application: UIApplication) {        
-        self.legacyAppDelegate.applicationWillResignActive(application)
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        legacyAppDelegate.applicationWillResignActive(application)
     }
-    
+
     func applicationDidEnterBackground(_ application: UIApplication) {
-        self.legacyAppDelegate.applicationDidEnterBackground(application)
+        legacyAppDelegate.applicationDidEnterBackground(application)
     }
-    
+
     func applicationWillEnterForeground(_ application: UIApplication) {
-        self.legacyAppDelegate.applicationWillEnterForeground(application)
+        legacyAppDelegate.applicationWillEnterForeground(application)
     }
-    
+
     func applicationWillTerminate(_ application: UIApplication) {
-        self.legacyAppDelegate.applicationWillTerminate(application)
+        legacyAppDelegate.applicationWillTerminate(application)
     }
-    
+
     func applicationDidReceiveMemoryWarning(_ application: UIApplication) {
-        self.legacyAppDelegate.applicationDidReceiveMemoryWarning(application)
+        legacyAppDelegate.applicationDidReceiveMemoryWarning(application)
     }
-    
+
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        return self.appCoordinator.open(url: url, options: options)
+        return appCoordinator.open(url: url, options: options)
     }
-    
+
     // MARK: User Activity Continuation
-    
+
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-        return self.legacyAppDelegate.application(application, continue: userActivity, restorationHandler: restorationHandler)
+        return legacyAppDelegate.application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
-    
+
     // MARK: Push Notifications
-    
+
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        self.legacyAppDelegate.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+        legacyAppDelegate.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
     }
-    
+
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        self.legacyAppDelegate.application(application, didFailToRegisterForRemoteNotificationsWithError: error)
+        legacyAppDelegate.application(application, didFailToRegisterForRemoteNotificationsWithError: error)
     }
-    
+
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        self.legacyAppDelegate.application(application, didReceiveRemoteNotification: userInfo, fetchCompletionHandler: completionHandler)
+        legacyAppDelegate.application(application, didReceiveRemoteNotification: userInfo, fetchCompletionHandler: completionHandler)
     }
 }
